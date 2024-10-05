44c8b755ce4a25b83c9ff73c4ecdc1dda9697506
muzeel <vasilets.4@mail.ru>
2024-10-05 17:42:40 +0300

    initial commit

---
+++ ExtraTask1/extra-task-1.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef EXTRA_TASK_1_H
+#define EXTRA_TASK_1_H
+#include <cassert>
+#include <iostream>
+
+//Return the number of seconds later that a time in seconds time_2 is than a time in seconds time_1.
+double seconds_difference(double, double);
+
+#endif // !EXTRA_TASK_1_H
