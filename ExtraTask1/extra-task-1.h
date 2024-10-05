92a66e88374c463b1452596d0e27efcdc44c136b
muzeel <vasilets.4@mail.ru>
2024-10-05 19:06:09 +0300

    hours_difference done

--- ExtraTask1/extra-task-1.h
+++ ExtraTask1/extra-task-1.h
@@ -7,4 +7,7 @@
 //Return the number of seconds later that a time in seconds time_2 is than a time in seconds time_1.
 double seconds_difference(double, double);
 
+//Return the number of hours later that a time in seconds time_2 is than a time in seconds time_1.
+double hours_difference(double, double);
+
 #endif // !EXTRA_TASK_1_H
