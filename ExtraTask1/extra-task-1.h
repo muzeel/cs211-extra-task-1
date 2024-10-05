2e71f6d72ee7c25c2abccf29f180405ae4c5e0fc
muzeel <vasilets.4@mail.ru>
2024-10-05 19:11:51 +0300

    to_float_hours done

--- ExtraTask1/extra-task-1.h
+++ ExtraTask1/extra-task-1.h
@@ -4,10 +4,13 @@
 #include <cassert>
 #include <iostream>
 
-//Return the number of seconds later that a time in seconds time_2 is than a time in seconds time_1.
+//Returns the number of seconds later that a time in seconds time_2 is than a time in seconds time_1.
 double seconds_difference(double, double);
 
-//Return the number of hours later that a time in seconds time_2 is than a time in seconds time_1.
+//Returns the number of hours later that a time in seconds time_2 is than a time in seconds time_1.
 double hours_difference(double, double);
+
+//Returns the total number of hours in the specified number of hours, minutes, and seconds.
+double to_float_hours(int, int, int);
 
 #endif // !EXTRA_TASK_1_H
