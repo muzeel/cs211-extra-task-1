1da5b5d18fa66d8ede3fd989aff041416edc5db4
muzeel <vasilets.4@mail.ru>
2024-10-05 19:37:34 +0300

    to_24_hour_clock done

--- ExtraTask1/extra-task-1.h
+++ ExtraTask1/extra-task-1.h
@@ -3,6 +3,7 @@
 #define EXTRA_TASK_1_H
 #include <cassert>
 #include <iostream>
+#include <cmath>
 
 //Returns the number of seconds later that a time in seconds time_2 is than a time in seconds time_1.
 double seconds_difference(double, double);
@@ -12,5 +13,8 @@ double hours_difference(double, double);
 
 //Returns the total number of hours in the specified number of hours, minutes, and seconds.
 double to_float_hours(int, int, int);
+
+//Returns the hour as seen on a 24 - hour clock.
+double to_24_hour_clock(double);
 
 #endif // !EXTRA_TASK_1_H
